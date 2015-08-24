73f8ff0de4f61c82b58ed6e3f95246d445d011ac
Daniel Choi <dhchoi@gmail.com>
2015-08-24 16:02:12 -0400

    fix

--- logpipe.sh
+++ logpipe.sh
@@ -1,2 +1,2 @@
 # cleans up logging output
-grep '\--->' | sed 's@ --->@@g' | awk '$8 == "PRIVMSG" { sub("!.*", "X", $7); printf "%s %s %s %s %s %s %-17s ", $1, $2, $3, $4, $5, $6, $7; for (i=10;i<=NF;i++) printf "%s ", $i;printf "\n" } ' 
+grep '\--->' | sed 's@ --->@@g' | awk '$8 == "PRIVMSG" { sub("!.*", ":", $7); printf "%s %s %s %s %s %s %-17s ", $1, $2, $3, $4, $5, $6, $7; for (i=10;i<=NF;i++) printf "%s ", $i;printf "\n" } ' 
